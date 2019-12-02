442837cdecbbab3e18db68f8811f05fdd6f880bf
selviyuaniva <56258731+selviyuaniva@users.noreply.github.com>
2019-12-02 13:19:17 +0700

    Add files via upload

---
+++ program 1-10.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << " \n menampilkan angka 1-10 \n" << endl;
+	int a=1;
+	while(a<=10){
+		cout << a << endl;
+		a++;
+	}
+	return 0;
+}
---
+++ program deret.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+	for(int n=100000000; n>=1; n=n/10){
+		cout << n << "\n";
+	}
+	return 0;
+}
+
